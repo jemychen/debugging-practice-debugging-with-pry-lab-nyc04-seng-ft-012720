69f126bac8c2c28c5625c976f77e6accab321e90
Jemy Chen <jemychen@live.com>
2020-01-23 16:35:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -10,7 +10,10 @@ class GasStation
     @brand = brand
     @unleaded_price = unleaded_price
   end
-
+  
+  def unleaded_price=(price)
+    @unleaded_price = price
+  end
 end
 
 petrol_petes = GasStation.new("Petrol Pete's", 50)
